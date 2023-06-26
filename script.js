6a5e6c9f50465958b9c459ce80b4c08de400601d
Ali Garjani <garjani.a@gmail.com>
2023-06-26 23:54:50 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// script.js
+
+// Typing effect for the title
+const titleElement = document.querySelector('.title');
+const text = 'Ali Garjani';
+let index = -1;
+
+function type() {
+  if (index === -1) {
+    titleElement.textContent = '';
+  } else {
+   titleElement.textContent += text[index];
+  }
+  index++;
+  if (index === text.length) {
+    titleElement.classList.remove('typing');
+    return;
+  }
+  setTimeout(type, 100);
+}
+
+// Trigger the typing effect when the page is loaded
+window.addEventListener('load', () => {
+  titleElement.classList.add('typing');
+  type();
+});
